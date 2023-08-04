a1b634dcafc77ee46794deaa0ff407ada5a60f58
Krrish <malekm09122003@gmail.com>
2023-08-04 15:42:05 +0530

    Updated Dropdown content

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -9,7 +9,7 @@ type dropdownProps = {
 const Dropdown = ({items,content,setContent}:dropdownProps) => {
 
   const dropItems = items.map((item,index) =>
-    <li key={index} className="py-1 px-4 text-black text-ellipsis font-sans text-xl overflow-x-hidden hover:bg-indigo-200 cursor-pointer max-sm:text-2xl"
+    <li key={index} className="py-1 px-4 text-black text-ellipsis whitespace-nowrap font-sans text-2xl overflow-x-hidden hover:bg-indigo-200 cursor-pointer max-sm:text-2xl"
     onClick={()=>{setDstate(!dstate);setContent(item);}}>
         {item}
     </li>
@@ -35,9 +35,9 @@ const Dropdown = ({items,content,setContent}:dropdownProps) => {
     <div className="mb-16 max-lg:mb-12 max-sm:px-6" ref={divRef}>
         <div className="shadow-2xl cursor-pointer text-ellipsis px-2 border-2 overflow-x-hidden border-black rounded py-2 w-60 bg-black text-white max-lg:w-96 max-sm:w-72 max-[340px]:w-60"
             onClick={()=>{setDstate(!dstate)}}>
-            <h3 className="font-sans text-3xl max-sm:text-2xl">{content}</h3>
+            <h3 className="font-sans text-3xl max-sm:text-2xl whitespace-nowrap mx-2 overflow-hidden">{content}</h3>
         </div>
-        <div className={`shadow-2xl z-10 bg-white absolute border-2 border-black rounded mt-2 py-2 w-60 max-h-64 overflow-y-auto ${!dstate && 'hidden'} max-lg:w-96 max-sm:w-72 max-[340px]:w-60 ${dstate && 'dd-forward'}`}>
+        <div className={`shadow-2xl bg-gray-100 absolute border-2 border-black rounded mt-2 py-2 w-60 max-h-64 overflow-y-auto ${!dstate && 'hidden'} max-lg:w-96 max-sm:w-72 max-[340px]:w-60 ${dstate && 'dd-forward'}`}>
             <ul>{dropItems}</ul>
         </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Github from "./../assets/Github.svg"
 
 const Footer = () => {
   return (
-    <div className="bg-black flex flex-row justify-center py-12 px-40 gap-32 items-center mt-20 max-xl:px-20 max-lg:px-0 max-lg:gap-16 max-sm:flex-col max-sm:items-center max-sm:gap-6  max-lg:mt-4">
+    <div className="bg-black flex flex-row justify-center py-12 px-40 gap-32 items-center mt-20 max-xl:px-20 max-lg:px-0 max-lg:gap-16 max-sm:flex-col max-sm:items-center max-sm:gap-6  max-lg:mt-4 absolute -z-10">
         <h2 className="text-light-gray font-normal font-sans text-3xl basis-1/3 max-sm:hidden">"Enabling data-driven projections for future vaccine procurement needs."</h2>
         <img className="h-20 cursor-pointer hover:translate-y-1" src={Github} alt="GitHub Logo" onClick={()=>{window.open('https://github.com/ota0912/Vaccine-Procurement-Predictor')}}/>
         <div className="text-right basis-1/3 max-sm:basis-0 max-sm:text-center">
